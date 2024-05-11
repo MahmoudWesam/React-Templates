fbc425a6218300a4029c59e4fd830eb3a6c53d99
Mahmoud Wesam <mah.wesaam@gmail.com>
2024-05-11 18:26:09 +0300

    refactor(linting): setting up linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,78 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es6: true,
+  },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
+    "react-app",
+    "plugin:prettier/recommended",
+    "prettier",
+    // "plugin:storybook/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
     "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
-  rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
-};
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["react", "react-refresh", "react-hooks"],
+  rules: {
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "react/prefer-stateless-function": 1,
+    "no-console": ["warn", { allow: ["warn", "error"] }],
+    "no-use-before-define": "off",
+    "no-param-reassign": "off",
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
+    "import/no-default-export": "error",
+    "react-refresh/only-export-components": "warn",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+  },
+  overrides: [
+    {
+      files: ["**/*.ts", "**/*.tsx"],
+      parser: "@typescript-eslint/parser",
+      extends: [
+        "react-app",
+        // "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended",
+      ],
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        ecmaVersion: 2018,
+        sourceType: "module",
+      },
+      rules: {
+        "react/jsx-filename-extension": [2, { extensions: [".js", ".jsx", ".ts", ".tsx"] }],
+        "no-use-before-define": "off",
+        "@typescript-eslint/no-use-before-define": ["error", { variables: false }],
+        "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_.*", varsIgnorePattern: "^_.*" }],
+        "@typescript-eslint/explicit-function-return-type": [
+          "error",
+          {
+            allowExpressions: true,
+          },
+        ],
+      },
+      settings: { react: { version: "detect" } },
+    },
+    // {
+    //   // override for storybook
+    //   files: ["*.{stories,story}.{ts,tsx}"],
+    //   rules: {
+    //     "@typescript-eslint/naming-convention": "off",
+    //     "@typescript-eslint/no-floating-promises": "off",
+    //     "import/no-default-export": "off",
+    //     "jsdoc/require-jsdoc": "off",
+    //   },
+    // },
+  ],
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import eslint from "vite-plugin-eslint"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-});
+  plugins: [
+    react(),
+    eslint({
+      exclude: ["/virtual:/**"],
+    }),
+  ],
+  build: { outDir: "build" },
+  server: { open: true, port: 3000 },
+})
